3be8fd4ef54cc2adc2b04a11642ce45272ef14ed
DepressoEsspresso <luigy199601@gmail.com>
2026-09-06 20:51:17 -0700

    Sept. 6

--- 6.9MiniProgrammingProject.cpp
+++ 6.9MiniProgrammingProject.cpp
@@ -7,47 +7,63 @@
 using namespace std;
 
 // Function prototypes
-// WRITE PROTOTYPES FOR THE displayMenu, getChoice,
-// kilosToPounds and poundsToKilos FUNCTIONS HERE.
 void displayMenu();
-int getChoice(int);
+int getChoice();
+double kilos2lbs();
+double lbs2kilos();
 
 /*****     main     *****/
 int main()
 {
-   // DECLARE ANY VARIABLES MAIN USES HERE.
-   
-   // WRITE THE CODE HERE TO CARRY OUT THE STEPS
-   // REQUIRED BY THE PROGRAM SPECIFICATIONS.
-     
+    cout << fixed << showpoint << setprecision(2);
+    int choice;
+    do {
+        displayMenu();
+        choice = getChoice();
+        switch (choice){
+        case 1 : 
+            cout << "Pounds = " << kilos2lbs() << endl; break;
+        case 2 : 
+            cout << "Kilos = " << lbs2kilos() << endl; break;
+        }
+
+  }while (choice != 3);  
+
+  cout << "Exiting Program\n";
+
    return 0;
 }
 void displayMenu(){
-    cout << "               MENU\n\n";
+    cout << "               MENU\n\n" << 
 
-    cout << "   1. Convert kilograms to pounds\n" <<
+            "   1. Convert kilograms to pounds\n" <<
             "   2. Convert pounds to kilograms\n" <<
             "   3. Quit\n";
 }
-
-/*****     getChoice     *****/
-int getChoice(int c){
+int getChoice(){ 
     int c;
     cin >> c;
     while (c < 1 || c > 3){
-        system("cls");  //screen clear
-        cout << "Invalid choice, please choose from the following options\n";
+        cout << "\n\nInvalid choice, please choose from the following options\n";
         displayMenu();
         cin >> c;
     }return c;
 }
-
-/*****     kilosToPounds     *****/
-// WRITE THE kilosToPounds FUNCTION HERE.
-// IT RECEIVES A WEIGHT IN KILOS AND MUST CALCULATE
-// AND RETURN THE EQUIVALENT NUMBER OF POUNDS.
-
-/*****    poundsToKilos     *****/
-// WRITE THE poundsToKilos FUNCTION HERE.
-// IT RECEIVES A WEIGHT IN POUNDS AND MUST CALCULATE
-// AND RETURN THE EQUIVALENT NUMBER OF KILOS.
+double kilos2lbs(){
+    double k;
+    double lbs; 
+    cout << "Kilos --> Pounds\n" <<
+            "How many kilos? ";
+    cin >> k;
+    lbs = k * 2.20462;
+    return lbs;
+}
+double lbs2kilos(){
+    double k;
+    double lbs; 
+    cout << "Pounds --> Kilos\n" <<
+            "How many pounds? ";
+    cin >> lbs;
+    k = lbs / 2.20462;
+    return k;
+}
